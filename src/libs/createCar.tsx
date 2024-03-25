0ff3ebd4c5f4b97375074ecbe5df4ed811ddd427
Nattapol Teerayuttawong <nattapol05@hotmail.com>
2024-03-25 23:29:46 +0700

    feat: add fetch api createCar function

---
+++ src/libs/createCar.tsx
@@ -0,0 +1,27 @@
+export default async function createCar(license:string, type:string, model:string, color:string[], fuel_type:string, year:number, price:number, mileage:number, condition:string, image:string[], number_of_seats:number, shopId:string, token:string) {
+    
+    const response = await fetch(`http://localhost:5000/api/v1/shops/${shopId}/cars`,{
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json",
+            authorization:`Bearer ${token}`
+        },
+        body: JSON.stringify({
+                license: license,
+                type: type,
+                model: model,
+                color: color,
+                fuel_type: fuel_type,
+                year: year,
+                price: price,
+                mileage: mileage,
+                condition: condition,
+                image: image,
+                number_of_seats: number_of_seats
+        }),
+    });
+    if(!response.ok){
+        throw new Error("Failed to fetch createCar")
+    }
+    return await response.json()
+}
