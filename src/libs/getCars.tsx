467127e06475609b758e2ff5a054a17aa8c1e9a4
Nattapol Teerayuttawong <nattapol05@hotmail.com>
2024-03-25 20:14:14 +0700

    fix: remove setTimeOut()

--- src/libs/getCars.tsx
+++ src/libs/getCars.tsx
@@ -1,7 +1,4 @@
 export default async function getCars() {
-
-    await new Promise((resolve)=>setTimeout(resolve,5000));
-
     const response = await fetch("http://localhost:5000/api/v1/cars");
     if(!response.ok) {
         throw new Error("Failed to fetch cars");
