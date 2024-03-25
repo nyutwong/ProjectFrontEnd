5a286002f58ad7366eca049347b057a4c628b0c8
Nattapol Teerayuttawong <nattapol05@hotmail.com>
2024-03-25 22:19:48 +0700

    feat: add fetch api getBooking function

---
+++ src/libs/getBooking.tsx
@@ -0,0 +1,14 @@
+export default async function getBookings(token:string,id:string) {
+    
+    const response = await fetch(`http://localhost:5000/api/v1/bookings/${id}`,{
+        method: "GET",
+        headers:{
+            authorization:`Bearer ${token}`
+        },
+    });
+
+    if(!response.ok){
+        throw new Error("Failed to fetch booking");
+    }
+    return await response.json()
+}
