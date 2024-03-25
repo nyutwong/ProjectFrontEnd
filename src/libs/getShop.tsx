f7f6ea748bc0366951fc58908a7a3e126a814fc1
Nattapol Teerayuttawong <nattapol05@hotmail.com>
2024-03-25 20:10:45 +0700

    feat: add fetch api getShop function

---
+++ src/libs/getShop.tsx
@@ -0,0 +1,7 @@
+export default async function getShop(id:string) {
+    const response = await fetch(`http://localhost:5000/api/v1/shops/${id}`);
+    if(!response.ok) {
+        throw new Error("Failed to fetch shop");
+    }
+    return await response.json();
+}
