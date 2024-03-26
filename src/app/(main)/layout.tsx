33dd4aeee186e4c3418018fefa427cde50f21aa5
Siratee Topradit <tae.siratee@gmail.com>
2024-03-26 15:18:59 +0700

    feat: Refactor layout component to add margin top to children

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,18 +1,16 @@
 import TopMenu from "@/components/TopMenu";
 import Footer from "@/components/Footer";
 
-
 export default async function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-
     return (
         <html lang="en">
             <body className={`{inter.className}`}>
                 <TopMenu />
-                {children}
+                <div className="mt-16">{children}</div>
                 <Footer />
             </body>
         </html>
