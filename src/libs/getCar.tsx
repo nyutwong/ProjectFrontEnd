3bcc54830a974bf10dd91385376f3113000d4cdb
Nattapol Teerayuttawong <nattapol05@hotmail.com>
2024-03-26 19:12:43 +0700

    fix: no-cached fetch

--- src/libs/getCar.tsx
+++ src/libs/getCar.tsx
@@ -1,5 +1,5 @@
 export default async function getCar(id:string) {
-    const response = await fetch(`https://mai-chob-kin-mala-backend.vercel.app/api/v1/cars/${id}`);
+    const response = await fetch(`https://mai-chob-kin-mala-backend.vercel.app/api/v1/cars/${id}`,{cache:'no-cache'});
     if(!response.ok) {
         throw new Error("Failed to fetch getCar");
     }
--- src/libs/getShops.tsx
+++ src/libs/getShops.tsx
@@ -1,5 +1,5 @@
 export default async function getShops() {
-    const response = await fetch(`https://mai-chob-kin-mala-backend.vercel.app/api/v1/shops`);
+    const response = await fetch(`https://mai-chob-kin-mala-backend.vercel.app/api/v1/shops`,{cache:'no-cache'});
     if(!response.ok) {
         throw new Error("Failed to fetch getShops");
     }
