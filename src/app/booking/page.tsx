a75d4aec5dad12b37152e04800647ceb961bcd55
Nattapol Teerayuttawong <nattapol05@hotmail.com>
2024-03-26 12:55:48 +0700

    feat: add each route sagment

---
+++ src/app/booking/page.tsx
@@ -0,0 +1,5 @@
+export default function Booking(){
+    return(
+        <></>
+    )
+}
--- src/app/reservations/manage/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ManageReservations(){
-    return(
-        <main className="text-center text-lg">
-            <div>Manage Your Reservation</div>
-        </main>
-    )
-}
