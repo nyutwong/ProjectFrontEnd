93dc3ba8cd37dfda23ce46ccd5fece075f2931ca
Nattapol Teerayuttawong <nattapol05@hotmail.com>
2024-03-26 17:37:55 +0700

    fix: remove CardPanal

--- src/app/(main)/car/page.tsx
+++ src/app/(main)/car/page.tsx
@@ -2,7 +2,6 @@ import CarCatalog from "@/components/CarCatalog";
 import getCars from "@/libs/getCars";
 import { Suspense } from "react";
 import { LinearProgress } from "@mui/material";
-import CarPanel from "@/components/CarPanel";
 
 export default function Car(){
     const cars = getCars();
@@ -13,10 +12,6 @@ export default function Car(){
             <Suspense fallback={<p>Loading...<LinearProgress/></p>}>
                 <CarCatalog carJson={cars}/>
             </Suspense>
-            
-            <hr className="my-10"/>
-            <h1 className="text-xl font-medium">Try Client-side Car Panel</h1>
-            {/* <CarPanel/> */}
         </main>
     )
 } 
