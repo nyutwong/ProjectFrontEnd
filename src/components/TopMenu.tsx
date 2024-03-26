73c1e4120e12cf9cb7566b5c88a3d5e041c8ac3d
Nattapol Teerayuttawong <nattapol05@hotmail.com>
2024-03-26 11:58:26 +0700

    feat: add session check

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -8,6 +8,7 @@ import { Link } from "@mui/material";
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { useSession } from "next-auth/react";
 
 const navigation = [
     { name: "Dashboard", href: "#", current: true },
@@ -21,7 +22,8 @@ function classNames(...classes: string[]) {
 }
 
 export default function TopMenu() {
-    // const session = await getServerSession(authOptions);
+  const {data:session} = useSession();
+  // const session = await getServerSession(authOptions);
 
     return (
         <Disclosure as="nav" className="bg-white dark:bg-gray-800">
@@ -104,7 +106,7 @@ export default function TopMenu() {
                                             </span>
                                             <img
                                                 className="h-8 w-8 rounded-full"
-                                                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                                src={session?"https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80":"https://www.shutterstock.com/image-vector/blank-avatar-photo-place-holder-600nw-1095249842.jpg"}
                                                 alt=""
                                             />
                                         </Menu.Button>
@@ -119,10 +121,44 @@ export default function TopMenu() {
                                         leaveTo="transform opacity-0 scale-95"
                                     >
                                         <Menu.Items className="absolute right-0 z-10 bg mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                            {
+                                              session? <Menu.Item>
+                                              {({ active }) => (
+                                                  <a
+                                                      href="#"
+                                                      className={classNames(
+                                                          active
+                                                              ? "bg-gray-100"
+                                                              : "",
+                                                          "block px-4 py-2 text-sm text-gray-700"
+                                                      )}
+                                                  >
+                                                      Your Profile
+                                                  </a>
+                                              )}
+                                          </Menu.Item>: null
+                                            }
+                                            {
+                                              session? <Menu.Item>
+                                              {({ active }) => (
+                                                  <a
+                                                      href="#"
+                                                      className={classNames(
+                                                          active
+                                                              ? "bg-gray-100"
+                                                              : "",
+                                                          "block px-4 py-2 text-sm text-gray-700"
+                                                      )}
+                                                  >
+                                                      Settings
+                                                  </a>
+                                              )}
+                                          </Menu.Item> : null
+                                            }
                                             <Menu.Item>
                                                 {({ active }) => (
                                                     <a
-                                                        href="#"
+                                                        href={session?"/api/auth/signout":"/api/auth/signin"}
                                                         className={classNames(
                                                             active
                                                                 ? "bg-gray-100"
@@ -130,37 +166,7 @@ export default function TopMenu() {
                                                             "block px-4 py-2 text-sm text-gray-700"
                                                         )}
                                                     >
-                                                        Your Profile
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <a
-                                                        href="#"
-                                                        className={classNames(
-                                                            active
-                                                                ? "bg-gray-100"
-                                                                : "",
-                                                            "block px-4 py-2 text-sm text-gray-700"
-                                                        )}
-                                                    >
-                                                        Settings
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <a
-                                                        href="#"
-                                                        className={classNames(
-                                                            active
-                                                                ? "bg-gray-100"
-                                                                : "",
-                                                            "block px-4 py-2 text-sm text-gray-700"
-                                                        )}
-                                                    >
-                                                        Sign out
+                                                        {session? "Sign out":"Sign in"}
                                                     </a>
                                                 )}
                                             </Menu.Item>
