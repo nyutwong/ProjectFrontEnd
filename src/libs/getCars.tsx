5f4ab9dea2cad7c48ca197de48f0afa086ce7298
Nattapol Teerayuttawong <nattapol05@hotmail.com>
2024-03-26 17:06:10 +0700

    fix: change fetch to no-cache

--- src/libs/getCars.tsx
+++ src/libs/getCars.tsx
@@ -1,5 +1,5 @@
 export default async function getCars() {
-    const response = await fetch("https://mai-chob-kin-mala-backend.vercel.app/api/v1/cars");
+    const response = await fetch("https://mai-chob-kin-mala-backend.vercel.app/api/v1/cars",{cache:'no-cache'});
     if(!response.ok) {
         throw new Error("Failed to fetch getCars");
     }
