dfa2e9e7da2df022f5c885c69c1f3a4db3b7393f
Siratee Topradit <tae.siratee@gmail.com>
2024-03-26 12:58:42 +0700

    feat: Add ThemeSwitcher component to TopMenu

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -9,6 +9,7 @@ import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import { useSession } from "next-auth/react";
+import ThemeSwitcher from "./ThemeSwitcher";
 
 const navigation = [
     { name: "Dashboard", href: "#", current: true },
@@ -83,19 +84,7 @@ export default function TopMenu() {
                                 </div>
                             </div>
                             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                                <button
-                                    type="button"
-                                    className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                                >
-                                    <span className="absolute -inset-1.5" />
-                                    <span className="sr-only">
-                                        View notifications
-                                    </span>
-                                    <BellIcon
-                                        className="h-6 w-6"
-                                        aria-hidden="true"
-                                    />
-                                </button>
+                                <ThemeSwitcher />
 
                                 <Menu as="div" className="relative ml-3">
                                     <div>
