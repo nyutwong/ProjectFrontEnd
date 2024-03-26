f9bca58ac1b26de99e1334366b8b87215c19e795
Siratee Topradit <tae.siratee@gmail.com>
2024-03-26 14:48:38 +0700

    fix: Update background color in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import TravelCard from "@/components/TravelCard";
 
 export default function Home() {
   return (
-    <main className="g bg-white">
+    <main className="g bg-white dark:bg-gray-800">
       <Banner/>
       <TravelCard/>
     </main>
